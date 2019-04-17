b0dba4b74b9eecfa140afbd822d40acfab9fbf6a
Mihály Barczi <barczi.mihaly@gmail.com>
2019-04-17 14:19:55 +0200

    Updates

---
+++ precache-manifest.40f2c619effd757ceaec56b97ed2ff91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c7ca57cc592bcb412e353a564374da9",
+    "url": "/nated/static/media/logo.2c7ca57c.PNG"
+  },
+  {
+    "revision": "eeea3d187217637b6e47323eca32d731",
+    "url": "/nated/static/media/landing-background.eeea3d18.jpg"
+  },
+  {
+    "revision": "4f28ce9b106d22566b08",
+    "url": "/nated/static/js/runtime~main.99aec7ce.js"
+  },
+  {
+    "revision": "9179af62ce53fa256f49",
+    "url": "/nated/static/js/main.c20a1b72.chunk.js"
+  },
+  {
+    "revision": "bb1765d50b39eb21c0d4",
+    "url": "/nated/static/js/2.4d0be1d1.chunk.js"
+  },
+  {
+    "revision": "9179af62ce53fa256f49",
+    "url": "/nated/static/css/main.30ac6ef6.chunk.css"
+  },
+  {
+    "revision": "f42c25a623c097442c2bf0feb2965ac7",
+    "url": "/nated/index.html"
+  }
+];
