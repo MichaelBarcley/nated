f7de532a4bbfe271a6b2eb9cf6a09c5d98bfefb2
Mihály Barczi <barczi.mihaly@gmail.com>
2019-04-30 12:45:12 +0200

    add companyPageData descriptions

--- src/Data/CompanyPageData.js
+++ src/Data/CompanyPageData.js
@@ -1,7 +1,28 @@
 const CompanyPageData = [
-    {name: 'Weboldal készítés', description: 'dunno, random string here lul'},
-    {name: 'Hálózat kiépítés', description: 'dunno, random string here lel'},
-    {name: 'Random cucc', description: 'dunno, random string here lulz'}
+    {name: 'Hálózat kialakítás', description: `Az informatikai rendszerek alappillérei a lokális számítógépes hálózatok. Megfelelõ kialakításuk, 
+    nagy kapacitásuk, üzembiztos mûködésük kiemelten fontos feladat, hiszen ezek teszik lehetõvé az alkalmazások, szoftverek kielégítõ üzemét. 
+    Célunk olyan számítógépes hálózatok kialakítása, melyek a lehetõ legnagyobb mértékben szolgálják ügyfeleink igényeit. Helyszíni felmérés alapján, 
+    különféle szempontokat figyelembe véve gondosan kezdjük el a tervezést. Egyeztetések után, kialakítjuk a végleges megoldást. Az Önök által is jóváhagyott 
+    terveket rövid idõn belül a gyakorlati kivitelezés követi. Az Informatikai hálózatkiépítés szolgáltatás összetevõi 
+    fizikai hálózatkiépítés /LAN, WAN/ router konfigurálás, tûzfal telepítés hálózati alkalmazások, Internet, Intranet, elektronikus levelezés tervezése, 
+    üzemeltetése, szükség esetén javítása a hálózati munkához szükséges szerverek üzembeállítása, üzemeltetése vírusvédelmi rendszerek kialakítása a hálózati 
+    környezetbe illeszkedõ PC konfigurációk összeállítása`},
+    {name: 'Hardver beszerzés', description: `Egy vállalatirányítási rendszer bevezetése nehéz feladat. Sokszor elkerülhetetlen egy- egy technológiaváltás 
+    (korszerűbb hardvereszközök beszerezése), ami komoly anyagi beruházásokat igényel, hogy a versenyben az Ön cége se maradjon le. A hardver részlegünk 
+    erre a problémára nyújt megoldást. Cégünk szakemberei, rendszerük hardverállományának feltérképezése után képesek arra, hogy a teljesítmény szempontjából 
+    kompromisszummentesen, ám költséghatékony módon oldják meg a nélkülözhetetlen hardvereszközök üzembe helyezését. Ezt a feladatot informatikai tanácsadóink 
+    az Ön cégének Informatikus szakembereivel együttműködve, vagy önállóan is képesek elvégezni. A számítógép beszerzésével, üzembe helyezésével még nem ért 
+    véget. A számítógépek üzemeltetése, csakúgy, mint a szoftveré karbantartást igényel. Cégünk erre a problémára is megoldást nyújt. A szoftver szállításán, 
+    karbantartásán, hardver eszközök beszerzésen túl a teljes informatikai rendszer karbantartását is vállaljuk.`},
+    {name: 'Informatikai ellátás', description: `Mai világunk nélkülözhetetlen feltétele a piacra jutásnak és ottmaradásnak a vállalkozás korszerű informatikai háttere. 
+    Ebben nyújt segítséget cégünk - kezdve a hardver kiválasztásától és annak üzembe helyezésén át a mindennapi használatig. Célunk ügyfeleink részére megbízható, 
+    biztonságos számítástechnikai architektúra kialakítása, legyen az egyetlen gép, vagy komplex vállalti hálózat. Vállalunk hálózatok telepítését, 
+    karbantartását, üzemeltetését. Hardver-Szoftver tanácsadás, beszerzés és javítás is szolgáltatásaink közé tartozik - 
+    akár helyben is megjavítjuk elromlott számítógépét!`},
+    {name: 'Weboldalak készítése', description: `Segítünk cégének megjelenni az egyik legnagyobb hírdetési felületen, az Interneten. 
+    Készíttesse el weblapját még ma! Az Internet segítségével növelheti cégének ismertségét, vállalkozása információit a legcélzottabban közvetítheti ügyfeleinek. 
+    Ne habozzon! Vegye fel velünk a kapcsolatot! Mindent egy helyen!!! - design/grafika, programozás, megjelentetés, domain tárhely regisztráció, oldal karbantartás, 
+    interaktív weboldalak, webbolt alkalmazás... - korrekten, pontosan, megfizethetően!!! Hogy cége a lehető legjobb helyezést érje el a keresők találati listáján!`}
 ];
 
 export default CompanyPageData;
