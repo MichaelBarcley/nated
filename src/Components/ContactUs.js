a5acf3612d0be17379df7bdfb8cdc4e9af477b68
Mihály Barczi <barczi.mihaly@gmail.com>
2019-04-30 11:47:30 +0200

    add content of contactus component

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
+import './ContactUs.css';
+
 const contactus = () => {
-    return <div className="contactus">Hello from Contact Us!</div>
+    return ( 
+        <div className="contactus">
+            <div className="interested">
+                <h2>Felkeltettük érdeklődését?</h2>
+                <p>Keressen minket bizalommal!</p>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <h3>Barczi Zsolt</h3>
+                    <p>Telefonszám: +36 20 489 70 80</p>
+                    <p>Email cím: <a href="mailto:zsbarczi@nated.hu">zsbarczi@nated.hu</a></p>
+                </div>
+                <div className="col">
+                    <h3>Dobos Imre</h3>
+                    <p>Telefonszám: +36 20 489 70 80</p>
+                    <p>Email cím: <a href="mailto:idobos@nated.hu">idobos@nated.hu</a></p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default contactus;
