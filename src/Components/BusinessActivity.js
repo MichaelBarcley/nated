c88b28ef0b1a54e5364dc0596b15915b5a021134
Mihály Barczi <barczi.mihaly@gmail.com>
2019-05-16 13:56:11 +0200

    refact(natedintro.js): put content in a box with 10% margin

--- src/Components/BusinessActivity.js
+++ src/Components/BusinessActivity.js
@@ -5,7 +5,6 @@ import './BusinessActivity.css'
 const businessActivity = (props) => {
     return (
         <div className="businessactivity" id="businessactivity">
-            <p>Hello from Business Activity!</p>
             <p>{props.name}</p>
             <p>{props.description}</p>
         </div>
